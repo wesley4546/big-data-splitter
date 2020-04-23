47e5e7d9ca4cf888ba97f6156cb4a9cef62fe1e0
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-04-23 00:53:30 -0400

    Update big-data-splitter-csv.R

--- big-data-splitter-csv.R
+++ big-data-splitter-csv.R
@@ -1,16 +1,19 @@
-# R method to make csvs based on Rows. Did rows because approx ` 60000 rows are 23 MB which are close to the max
-# size allowed on github upload. 
+#author: Mihir Patel(opendatasurgeon )
+#last edit: Cleaned up comments
+
+# R method to make multiple csvs based on Rows.
 
 library(tidyverse)
 
-bigdf_data <- readr::read_csv("xyz.csv") #Read in the original csv file
+#Read in the original csv file
+bigdf_data <- readr::read_csv("xyz.csv") 
 
-#break the large CSV so RAM and Rstudio doesn't crash
+# Here I want 60000 rows per file until last row is reached
+# this method forces the execution from innermost, outer () opttional
+groups <- (split(bigdf_data, (seq(nrow(bigdf_data))-1) %/% 60000))
 
-groups <- (split(bigdf_data, (seq(nrow(bigdf_data))-1) %/% 60000)) #here I want 60000 rows per file until last row is reached
-
+#loop through end of each split, and write file with i
 for (i in seq_along(groups)) {
-  write.csv(groups[[i]], paste0("bigdf_data_output_file", i, ".csv")) #iterate and write file
+  write.csv(groups[[i]], paste0("bigdf_data_output_file", i, ".csv")) 
 }
-
-# all the files will be in the current directory. 
+#Output: all the files will be in the current directory. 
